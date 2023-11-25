f832483de5e2d7e3cd1cbb6f9fc601842a65c75f
Stefan Galler <galler.stefan@gmail.com>
2023-11-25 11:06:21 +0100

    extend example

--- packages/spacing_generator/example/lib/spacings.dart
+++ packages/spacing_generator/example/lib/spacings.dart
@@ -8,3 +8,21 @@ class Spacings {
   static const tiny = 2.0;
   static const small = 4.0;
 }
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: context.paddings.allSmall,
+      child: Column(
+        children: [
+          const Text('Hello'),
+          context.spaces.verticalTiny,
+          const Text('World'),
+        ],
+      ),
+    );
+  }
+}
